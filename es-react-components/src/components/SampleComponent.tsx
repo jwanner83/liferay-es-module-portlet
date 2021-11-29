7dd84b26100ce55a82643e946743fd056e1e2096
Jonas Wanner <jonas.wanner@clavisit.com>
2021-11-29 15:30:23 +0100

    feat: include sample component

---
+++ es-react-components/src/components/SampleComponent.tsx
@@ -0,0 +1,9 @@
+import { React } from 'es-react-provider'
+
+export default function SampleComponent () {
+    return (
+        <>
+            <h1>Sample Component</h1>
+        </>
+    )
+}
---
+++ es-react-components/src/index.tsx
@@ -0,0 +1,5 @@
+import SampleComponent from './components/SampleComponent'
+
+export {
+    SampleComponent
+}
