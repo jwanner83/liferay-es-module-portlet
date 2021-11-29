c822209142eeaf46acc5c948903ed60b07b496e6
Jonas Wanner <jonas.wanner@clavisit.com>
2021-11-29 14:43:08 +0100

    feat: adding es-react-provider

---
+++ es-react-provider/rollup.config.js
@@ -0,0 +1,31 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import esbuild from 'rollup-plugin-esbuild'
+import replace from '@rollup/plugin-replace'
+
+export default [
+  {
+    input: 'src/index.tsx',
+    output: {
+      format: 'esm',
+      dir: 'assets/lib'
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      esbuild(),
+      replace({
+        preventAssignment: true,
+        'process.env.NODE_ENV': JSON.stringify('production')
+      })
+    ]
+  },
+  {
+    input: 'src/entry.js',
+    output: {
+      format: 'commonjs',
+      exports: 'default',
+      file: 'build/entry.js'
+    }
+  }
+]
---
+++ es-react-provider/src/entry.js
@@ -0,0 +1,5 @@
+export default function main() {
+   console.group('es-react-provider')
+   console.info('this module provides packages as es-modules and isn`t intended to be placed directly.')
+   console.groupEnd()
+}
